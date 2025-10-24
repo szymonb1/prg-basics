4b8f05cb7634d9786c3073e2130fe350e6087b18
szymonbak <szymonbak144@gmail.com>
2025-10-24 12:55:45 +0200

    python 24.10.2025

---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,4 @@
+import math
+height = input("Podaj wysokość: ")
+d = 3.57 * math.sqrt(float(height)) 
+print(f"odległość od horyzontu wynosi: {d}")
---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,7 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+age = 18
+height = 1.71
+print(f"My name is {name}.\nI am {age} years old, and my height is {height} cm.\n In 6 years I will be {age +6} years old.  ")
